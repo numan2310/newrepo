0b0451fa6328cfbb28763921ff05ea1d4231d75f
numan2310 <pathan23n@gmail.com>
2025-08-20 12:29:06 +0530

    Add files via upload

--- newrepo-main/my-sample-app (1)/src/main/java/com/example/App.java
+++ newrepo-main/my-sample-app (1)/src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to maven123");
         Calculator calc = new Calculator();
-        int result = calc.add(8, 15);
-        System.out.println("8 + 15 = " + result);
+        int result = calc.add(5, 10);
+        System.out.println("5 + 10 = " + result);
     }
 }

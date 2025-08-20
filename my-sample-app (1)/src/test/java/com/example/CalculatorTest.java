818851eb18c8406ec2dd43b06edc04f8d7842ec3
numan2310 <pathan23n@gmail.com>
2025-08-20 11:57:26 +0530

    Update CalculatorTest.java

--- my-sample-app (1)/src/test/java/com/example/CalculatorTest.java
+++ my-sample-app (1)/src/test/java/com/example/CalculatorTest.java
@@ -7,12 +7,12 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calc = new Calculator();
-        assertEquals(15, calc.add(10, 5));
+        assertEquals(15, calc.add(15, 8));
     }
 
     @Test
     public void testSubtract() {
         Calculator calc = new Calculator();
-        assertEquals(5, calc.subtract(10, 5));
+        assertEquals(5, calc.subtract(15, 8));
     }
 }
